b0b2aad197f70a7c833c44d1e208b73c8eea5289
hanzandreiii <165552265+hanzandreiii@users.noreply.github.com>
2024-07-28 20:05:24 +0800

    Update map_services.dart

--- lib/map_services.dart
+++ lib/map_services.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 class MapServices {
-  final String key = 'AIzaSyBwgNnXzC8d7TffWAEkqpHeBvdWPUeEjqk';
+  final String key = 'YOUR_KEY_HERE';
   final String types = 'geocode';
 
   Future<List<AutoCompleteResult>> searchPlaces(String searchInput) async {
